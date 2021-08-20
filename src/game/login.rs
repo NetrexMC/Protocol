b88dd766839b78a3e89f50090cad3586964389b7
Olybear9 <olybear9@gmail.com>
2021-08-19 21:11:55 -0500

    chore: push local changes with login packet

--- src/game/login.rs
+++ src/game/login.rs
@@ -1,6 +1,16 @@
-use binary_utils::BinaryStream;
+use binary_utils::{BinaryStream, IBinaryStream, IBufferRead};
+use crate::util::{ProtocolDecoder};
 
 pub struct Login {
      pub protocol: u8,
      pub connection_request: BinaryStream
+}
+
+impl ProtocolDecoder for Login {
+     fn read(stream: &mut BinaryStream) -> Self {
+          Self {
+               protocol: stream.read_byte(),
+               connection_request: BinaryStream::init(&stream.read_slice_exact(None))
+          }
+     }
 }
