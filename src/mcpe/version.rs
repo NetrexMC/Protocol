19aef60e6153a7e4958a3a04bc847a42d045316f
Bavfalcon9 <olybear9@gmail.com>
2022-12-24 17:07:06 -0600

    chore: Update versions.json

--- src/mcpe/version.rs
+++ src/mcpe/version.rs
@@ -3,16 +3,42 @@
 // To add versions, add a version to the resources/versions.json file!
 
 /// The current minecraft patch version (1.10.2.x)
-pub const CURRENT_PATCH: u32 = 491;
+pub const CURRENT_PATCH: u32 = 563;
 /// The current minecraft minor version (1.10.x)
-pub const CURRENT_MINOR: u32 = 475;
+pub const CURRENT_MINOR: u32 = 560;
 /// The current minecraft major version (1.x.0)
-pub const CURRENT_MAJOR: u32 = 475;
+pub const CURRENT_MAJOR: u32 = 526;
 
 /// This is a helper enum to get a version number from a version string.
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 #[repr(u32)]
 pub enum Versions {
+    V1_19_60_24 = 563,
+    V1_19_60_23 = 562,
+    V1_19_60_20 = 561,
+    V1_19_51 = 560,
+    V1_19_50 = 559,
+    V1_19_50_20 = 558,
+    V1_19_41 = 557,
+    V1_19_40_22 = 555,
+    V1_19_40_20 = 554,
+    V1_19_30_25 = 553,
+    V1_19_30_22 = 552,
+    V1_19_30_20 = 551,
+    V1_19_22 = 545,
+    V1_19_20 = 544,
+    V1_19_20_23 = 543,
+    V1_19_20_22 = 542,
+    V1_19_11 = 534,
+    V1_19_10_22 = 533,
+    V1_19_10_21 = 532,
+    V1_19_10_20 = 530,
+    V1_19_2 = 527,
+    V1_19_0 = 526,
+    V1_19_0_34 = 524,
+    V1_19_0_26 = 516,
+    V1_19_0_24 = 514,
+    V1_19_0_20 = 512,
     V1_18_20_24 = 491,
     V1_18_20_23 = 490,
     V1_18_10_28 = 486,
